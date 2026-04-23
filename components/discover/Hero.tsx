415868cc64b3af74673264d686ae8b6fecb19f9a
DCabahug1 <150985638+DCabahug1@users.noreply.github.com>
2026-04-23 00:00:12 -0700

    Add gradient to discover page hero background image

--- components/discover/Hero.tsx
+++ components/discover/Hero.tsx
@@ -156,7 +156,7 @@ function Hero({
         />
       </motion.div>
 
-      <div className="absolute inset-0 -z-10 bg-linear-to-t from-black/70 to-transparent" />
+      <div className="absolute inset-0 -z-10 bg-linear-to-t from-black/85 via-black/60 to-black/40" />
 
       <motion.div
         className="flex flex-col items-center gap-3 text-center"
